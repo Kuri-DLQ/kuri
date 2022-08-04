b47e7a9c5ea0c5dcef48ae9abdd71587b5cd8052
Arjun Rasodha <arjunrasodha10@gmail.com>
2022-08-03 21:55:50 -0400

    createZipFile __dirname added
    Co-authored-by: Tony Beere <tonybeere87@gmail.com>
    Co-authored-by: Anna Kolliakou <akolliakou@gmail.com>
    Co-authored-by: Kris Ingva <krisingva@gmail.com>

--- aws_infrastructure/aws/lambda/createZipFile.js
+++ aws_infrastructure/aws/lambda/createZipFile.js
@@ -4,30 +4,34 @@ import fs from 'fs';
 const publishToSnsLambdaZip = new JSZip();
 const writeToDynamoLambdaZip = new JSZip();
 const postToSlackLambdaZip = new JSZip();
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export const createZipFiles = async () => {
   return new Promise(async (resolve, reject) => {
     setTimeout(async () => {
       try {
-        const publishToSnsLambda = fs.readFileSync('./aws_infrastructure/aws/lambda/handlers/publishToSnsLambda.js');  
+        const publishToSnsLambda = fs.readFileSync(__dirname + '/handlers/publishToSnsLambda.js');
         publishToSnsLambdaZip.file("publishToSnsLambda.js", publishToSnsLambda);
         const zippedSnsLambda = await publishToSnsLambdaZip.generateAsync({ type: 'nodebuffer', streamFiles: true });
-        fs.writeFileSync('./aws_infrastructure/aws/lambda/handlers/publishToSnsLambda.js.zip', zippedSnsLambda);
-  
-        const writeToDynamoLambda = fs.readFileSync('./aws_infrastructure/aws/lambda/handlers/writeToDynamoLambda.js');
+        fs.writeFileSync(__dirname + '/handlers/publishToSnsLambda.js.zip', zippedSnsLambda);
+
+        const writeToDynamoLambda = fs.readFileSync(__dirname + '/handlers/writeToDynamoLambda.js');
         writeToDynamoLambdaZip.file("writeToDynamoLambda.js", writeToDynamoLambda);
         const zippedDynamoLambda = await writeToDynamoLambdaZip.generateAsync({ type: 'nodebuffer', streamFiles: true });
-        fs.writeFileSync('./aws_infrastructure/aws/lambda/handlers/writeToDynamoLambda.js.zip', zippedDynamoLambda);
-  
-        const postToSlackLambda = fs.readFileSync('./aws_infrastructure/aws/lambda/handlers/postToSlackLambda.js');
+        fs.writeFileSync(__dirname + '/handlers/writeToDynamoLambda.js.zip', zippedDynamoLambda);
+
+        const postToSlackLambda = fs.readFileSync(__dirname + '/handlers/postToSlackLambda.js');
         postToSlackLambdaZip.file("postToSlackLambda.js", postToSlackLambda);
         const zippedSlackLambda = await postToSlackLambdaZip.generateAsync({ type: 'nodebuffer', streamFiles: true });
-        fs.writeFileSync('./aws_infrastructure/aws/lambda/handlers/postToSlackLambda.js.zip', zippedSlackLambda);
+        fs.writeFileSync(__dirname + '/handlers/postToSlackLambda.js.zip', zippedSlackLambda);
 
         resolve()
       } catch (err) {
           reject(err);
       }
     }, 10000)
-  });  
+  });
 }
